b440e835742af89000799125ad9a6086738cfde5
lhwdev <lhwdev6@outlook.com>
2026-04-06 16:03:00 +0900

    sdfsfqwefwefgwefg

---
+++ 26-sys-hpfp/hpfp.c
@@ -0,0 +1,35 @@
+#include "hpfp.h"
+#include <stdlib.h>
+
+hpfp int_converter(int input){
+
+}
+
+int hpfp_to_int_converter(hpfp input){
+}
+
+hpfp float_converter(float input){
+}
+
+float hpfp_to_float_converter(hpfp input){
+}
+
+hpfp addition_function(hpfp a, hpfp b){
+
+}
+
+hpfp multiply_function(hpfp a, hpfp b){
+
+}
+
+char* comparison_function(hpfp a, hpfp b){
+
+}
+
+float float_flipper(float input){
+
+}
+
+char* hpfp_to_bits_converter(hpfp result){
+
+}
